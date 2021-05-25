2991afd93d4fb25fe271a48e702e960db5520951
chaweewatp <chaweewat.p@gmail.com>
2021-05-25 19:59:50 +0700

    edit main10.go3

--- main10.go
+++ main10.go
@@ -14,7 +14,7 @@ type AboutData struct {
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Welcome to my home mama")
+		return c.String(http.StatusOK, "Welcome to my home papa")
 	})
 
 	e.GET("/about", aboutHandler)
