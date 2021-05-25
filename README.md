911ecde7f32739aea9303e571e7cdd6eede56889
chaweewatp <chaweewat.p@gmail.com>
2021-05-25 17:56:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoEchoJenkins
+# GoEchoJenkins
---
+++ main10.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "net/http"
+  "github.com/labstack/echo/v4"
+)
+
+type AboutData struct {
+  ID string
+  Name string
+}
+
+func main(){
+e := echo.New()
+e.GET("/", func(c echo.Context) error{
+  return c.String(http.StatusOK, "Hello World!")
+})
+
+e.GET("/about", aboutHandler)
+
+e.Logger.Fatal(e.Start(":1323"))
+}
+
+func aboutHandler(c echo.Context) error{
+  about := AboutData{
+    ID: "400050",
+    Name : "John",
+  }
+  return c.JSON(http.StatusOK, about)
+}
